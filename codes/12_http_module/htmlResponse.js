6828016cb7287d63cce9241859fcba21f6ce05fc
Colorfuls <mint.rosetta2001@gmail.com>
2024-02-12 01:37:36 +0700

    lecture - html response with stream

---
+++ codes/12_http_module/htmlResponse.js
@@ -0,0 +1,23 @@
+const http = require("node:http");
+const fs = require("node:fs");
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, {
+        "Content-Type": "text/html"
+    });
+
+    /* 
+        โดยปกติ pipe จะรับ writable destination (ตำแหน่งที่สามารถเขียนข้อมูลได้)
+        
+        โดย res นี้ก็เป็น writable destination เหมือนกัน โดยจะเป็นการเขียนเนื้อหาลงในเว็บเพจ
+        ก่อนส่งไปที่ client 
+
+        โดยใน code นี้ จะเป็นการค่อย ๆ อ่านข้อมูล index.html และส่งไปที่ client ทีละ chunk
+    */
+    fs.createReadStream(__dirname + "/index.html", {
+        encoding: "utf-8",
+        highWaterMark: 1
+    }).pipe(res);
+});
+
+server.listen(3000, () => console.log("Server listing at http://localhost:3000"));
