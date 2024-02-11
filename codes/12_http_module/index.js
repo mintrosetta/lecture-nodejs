cbee96dacc86a966fc963e9a8fa982528e6014f2
Colorfuls <mint.rosetta2001@gmail.com>
2024-02-11 19:06:48 +0700

    lecture - json

--- codes/12_http_module/index.js
+++ codes/12_http_module/index.js
@@ -1,10 +1,18 @@
 const http = require("node:http");
 
 const server = http.createServer((req, res) => {
+    const user = {
+        username: "Mint Rosetta",
+        age: 23
+    };
+    
     res.writeHead(200, {
-        "Content-Type": "text/plain"
+        "Content-Type": "application/json"
     });
-    res.end("Hello World");
+    
+    // JSON.stringify แปลง javascript object เป็น json string
+    // JSON.parse แปลง json string เป็น javascript object
+    res.end(JSON.stringify(user));
 });
 
 server.listen(3000, () => {
